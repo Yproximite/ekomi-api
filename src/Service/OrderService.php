6dbba6104c946cc66c77ac4a0d86f4cdee4e7bf5
RomulusED69 <redieux@gmail.com>
2019-12-17 11:26:20 +0100

    ekomi api limit result to 200

--- src/Service/OrderService.php
+++ src/Service/OrderService.php
@@ -21,6 +21,13 @@ class OrderService extends AbstractService implements ServiceInterface
         $path = 'orders';
 
         $response = $this->getClient()->sendRequest('GET', $path, $message->build());
+        $data     = $response['data'];
+
+        while ($response['limit'] > $response['total'] && $response['total'] > $message->getOffset() + 200) {
+            $message->setOffset($message->getOffset() + 200);
+            $response = $this->getClient()->sendRequest('GET', $path, $message->build());
+            $data     = array_merge($data, $response['data']);
+        }
 
         /** @var Order[] $models */
         $models = $this->getModelFactory()->createMany(Order::class, $response['data']);
